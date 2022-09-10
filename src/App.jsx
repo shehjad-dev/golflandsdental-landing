b1e8c3a6f90b296b1d6d213ae2793e03950349e5
shehjad-dev <satauskz@gmail.com>
2022-09-11 03:07:24 +0600

    inti: coding files intialized

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="App">
+            <h1 className="text-3xl font-bold underline hover:text-violet-400 transition-all duration-150">
+                Hello world!
+            </h1>
+        </div>
+    );
+}
+
+export default App;
